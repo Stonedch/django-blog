a3c9480a11d76ee04588e52791897ff42c41e325
Maxim Mukhin <stch.max.muhin@gmail.com>
2020-07-22 10:12:16 +0300

    chore: add parameters for the admin-panel of the "blog" app

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,13 @@ from . import models
 
 from django.contrib import admin
 
-admin.site.register(models.Post)
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "publish", "status")
+    list_filter = ("status", "created", "publish", "author")
+    search_fields = ("title", "body")
+    prepopulated_fields = {"slug": ("title", )}
+    raw_id_fields = ("author", )
+    date_hierarchy = "publish"
+    ordering = ("status", "publish")
